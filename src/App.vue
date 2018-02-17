1df8fa7c9ec625e59b0e2313b9f02f60472b4595
Beck <903451510@qq.com>
2018-02-17 19:14:28 +0800

    add Agv.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
+     <Agv/>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-  }
+import Agv from './components/Agv'
+export default {
+   name: 'app',
+   components: {
+  Agv
+}
+ }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    height: 100%;
-    /*margin-top: 60px;*/
-  }
 
-  body {
-    margin: 0 !important;
-  }
 </style>
